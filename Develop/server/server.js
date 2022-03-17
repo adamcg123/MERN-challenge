26a03e999f040262a169157e6e41ca380b2ce465
adamcg123 <adam.Gondorchin26@gmail.com>
2022-03-16 23:24:14 -0500

    almost done ready for heroku

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -29,26 +29,6 @@ app.use(express.json());
 
 
 
-// const startServer = async () => {
-//   // create a new Apollo server and pass in our schema data
-
-
-//   // Start the Apollo server
-//   await server.start();
-
-//   // integrate our Apollo server with the Express application as middleware
-
-
-//   // log where we can go to test our GQL API
-//   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-// };
-
-// // Initialize the Apollo server
-// startServer();
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html'))
-// });
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
